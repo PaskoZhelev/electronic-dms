10f01d4fc4d9c2da3b652882175f272a5092aa5a
PaskoZhelev <pasko92@abv.bg>
2017-10-31 23:44:10 +0100

    First Commit

---
+++ electronic-dms/src/main/java/com/pmz/personal/portfolio/ElectronicDmsApplication.java
@@ -0,0 +1,12 @@
+package com.pmz.personal.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicDmsApplication.class, args);
+	}
+}
